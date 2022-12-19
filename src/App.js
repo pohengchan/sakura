45766b8da4b7ca8475510678e89caeb128bb91cb
pohengchan <pohengchan@gmail.com>
2022-12-19 17:31:38 +0100

    display cards, hover, modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import './App.css';
 import { Link } from 'react-router-dom';
+import DataFetching from './components/DataFetching';
 
 function App() {
+// const dataArray = [];  
+// const uri = 'https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/55';
+// const initDetails = {
+//     method: 'get',
+//     headers: {"Content-Type": "application/json; charset=utf-8"},
+//     mode: "cors" 
+// };
+
+//   let card = null;
+//   async function getData(){
+//       const response = await fetch( uri, initDetails );
+//       card = await response.json();
+//        console.log("print " + card.sakuraCard )
+//       dataArray.push(card)
+//       console.log("print " + dataArray.length )     
+//       return dataArray;
+//   }
+ 
+// getData();
+//  setTimeout(() => {  console.log(card.englishName); }, 5000);
   return (
-    <>
-      <h1>Home Page</h1>
+    <div className="App">
+      
+      <header>
+        <h1>SAKURA TAROT</h1>
+        <h2>Choose 3 cards for your fortune!</h2>
+        <p>this is some card text</p>
+        <DataFetching/>
+      </header>
+
       <Link to='/fortune'>Go to your fortune</Link>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/DataFetching.js
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react"
+import React from 'react'
+import axios from "axios"
+
+import Modal from "react-modal";
+Modal.setAppElement("#root");
+
+function DataFetching() {
+    const [cards,setCards] = useState([])
+
+    useEffect(() => {
+        axios.get('https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/')
+        .then(res => {
+            console.log(res)
+            setCards(res.data)
+        })
+        .catch(err=> {
+            console.log(err)
+        })
+    }, [])
+
+    //Modal
+    const [isOpen, setIsOpen] = useState(false);
+    
+    //var engName = "";
+    
+    function toggleModal(englishName) {
+      setIsOpen(!isOpen);
+      //engName = englishName;
+    }
+    //Modal
+
+    //Hover
+    const [showText, setShowText] = useState(false)
+    const handleMouseEnter = e => {
+        e.target.style.background = "maroon"
+        setShowText(true)
+      }
+      const handleMouseLeave = e => {
+        e.target.style.background = "lightgoldenrodyellow"
+        setShowText(false)
+      }
+      //Hover
+
+  return (
+    <div>
+        {showText && <p className="message">Hovering</p>}
+        {
+        //cards.map(card => <li key={card.id}>{card.english}</li>)
+        cards.map(card =><img key={card.id} src="https://i.ibb.co/LJSmQ4f/Reverso-Clow.jpg" className="Card-Reverse" style={{cursor:'pointer'}} alt={`card face down ${card.englishName}`} onClick={toggleModal} onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}/>)
+        }
+        
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={toggleModal}
+            contentLabel="My dialog"
+        >
+            <h3>You have turned over </h3>
+            <p>This represents your past.</p>
+            <p>Turn over another card.</p>
+            <button onClick={toggleModal}>OK</button>
+      </Modal>
+    </div>
+  )
+}
+
+export default DataFetching
+//https://www.youtube.com/watch?v=bYFYF2GnMy8
+//https://www.codingdeft.com/posts/react-on-hover/#displaying-a-text-when-the-button-is-hovered
